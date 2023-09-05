7e7a811ddbe8e9813f82bed2678c965112030590
niccov <niccoventuri27@gmail.com>
2023-09-05 16:16:56 +0200

    esercizio completato

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] cibiPreferiti = { "Pizza", "Pasta", "Gelato", "Formaggio", "Lasagna", "Panelle", "Pollo"};
+
+//stampare la lunghezza della classifica
+
+Console.WriteLine("La classifica è lunga " + cibiPreferiti.Length + " elementi");
+
+//stampare la lunghezza della classifica
+
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i] + " " + (i+1) + " posto");
+}
+
+//Il mio cibo top
+
+Console.WriteLine("Il mio cibo preferito è " + cibiPreferiti[0]);
+
+// Il mio cibo preferito ma non troppo
+
+Console.WriteLine("Il mio cibo preferito ma non troppo è " + cibiPreferiti[cibiPreferiti.Length-1]);
+
+//Stampare il cibo che si trova nella posizione mediana gestendo il caso di elementi pari.
+
+if(cibiPreferiti.Length % 2 == 0)
+{
+    Console.WriteLine($"La classifica non ha una mediana");
+} else
+{
+    Console.WriteLine($"La mediana di questa classifica è {cibiPreferiti[cibiPreferiti.Length / 2]}");
+}
